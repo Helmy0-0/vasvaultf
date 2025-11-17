c50babf3beff94fdc29fdf54a302c3d0c9b7e90b
wignn <tigfiragnafatur1933@gmail.com>
2025-11-17 20:11:31 +0700

    fix

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-package main //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+package main
+
 import (
 	"github.com/joho/godotenv"
 )
@@ -8,4 +8,5 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic("Error loading .env file")
 	}
+
 }
